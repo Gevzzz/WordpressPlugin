6fe1b9e0b379ce7b977e8cdbcf740385dd08734b
Gevorg Minasyan <55835135+Gevzzz@users.noreply.github.com>
2021-11-29 02:37:34 -0600

    Update add-ogm-toorder.php

--- add-ogm-toorder.php
+++ add-ogm-toorder.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * Plugin Name: add-ogm-toorder
- * Description: add-ogm-toorder
+ * Plugin Name: setOGM
+ * Description: setOGM
  * Version: 1.1
- * Text Domain: add-ogm-toorder
+ * Text Domain: setOGM
  * Author: Gevorg Minasyan
  */
  
- add_action( 'woocommerce_new_order', 'add_ogm_to_order' );
-function add_ogm_to_order( $order_id ) {
+ add_action( 'woocommerce_new_order', 'setOGM' );
+function setOGM( $order_id ) {
     $ogm = calculateOGM( $order_id );
     update_post_meta($order_id,'OGM',$ogm);
 }
 
 function calculateOGM($number)
 {
-$ordernumber = str_pad($number,10,"0");
+$ordernumber = str_pad($number,10,"0",STR_PAD_LEFT);
 $ordernumber = intval($ordernumber);
 $remainder =  $ordernumber%97;
 if($remainder ==0 )
 {
 $remainder = 97;
 }
-return $tempoutput = strval($ordernumber).strval($remainder);
+return $tempoutput = str_pad($number,10,"0",STR_PAD_LEFT).strval(str_pad($remainder,2,"0",STR_PAD_LEFT));
 
 }
